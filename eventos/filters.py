a97197e4b415ed5126c5e58f3d6fed628d899aaa
luziana <luzi.feitosa@hotmail.com>
2018-10-23 13:28:17 -0300

    filtro deficiente

--- eventos/filters.py
+++ eventos/filters.py
@@ -5,7 +5,7 @@ from .models import Inscricao
 class InscricaoFilter(FilterSet):
     class Meta:
         model = Inscricao
-        fields = ('nome', 'tipo', 'validado', 'alojamento','pagamento','cidade','uf')
+        fields = ('nome', 'tipo', 'validado', 'alojamento','pagamento','cidade','uf','deficiente')
 
     nome = CharFilter(
         field_name='usuario',
