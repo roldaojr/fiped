2ffb1ab1ab58d4dff5d67e5d2921aacc8bd8e57f
luziana <luzi.feitosa@hotmail.com>
2018-10-23 11:42:50 -0300

    adicionando filter cidade uf

--- eventos/filters.py
+++ eventos/filters.py
@@ -5,7 +5,7 @@ from .models import Inscricao
 class InscricaoFilter(FilterSet):
     class Meta:
         model = Inscricao
-        fields = ('nome', 'tipo', 'validado', 'alojamento','pagamento')
+        fields = ('nome', 'tipo', 'validado', 'alojamento','pagamento','cidade','uf')
 
     nome = CharFilter(
         field_name='usuario',
