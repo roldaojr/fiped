9e5ea2a82a8f067ea8a8516a9363bec750f0f225
luziana <luzi.feitosa@hotmail.com>
2018-11-24 18:13:45 -0300

    adicionando comentario

--- eventos/views/__init__.py
+++ eventos/views/__init__.py
@@ -21,8 +21,6 @@ class ImprimirLista(TableListView):
     def get_table_class(self):
         extra = {
             'nome_completo': Column(accessor='usuario.nome_completo'),
-            'nome_social': Column(accessor='usuario.nome_social'),
-            'cpf': Column(accessor='usuario.cpf'),
             'email': Column(accessor='usuario.email')
         }
         return table_factory(self.model, self.list_display, action=None,
