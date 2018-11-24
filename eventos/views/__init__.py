39cc3eab57b7fec7fb6c167881c27966eeb037ed
luziana <luzi.feitosa@hotmail.com>
2018-11-24 18:17:51 -0300

    adicionando filtro

--- eventos/views/__init__.py
+++ eventos/views/__init__.py
@@ -14,15 +14,17 @@ from ..filters import InscricaoFilter
 
 class ImprimirLista(TableListView):
     filterset_class = InscricaoFilter
-    list_display = ('nome_completo', 'email', ' assinatura')
+    list_display = ('nome_completo', 'nome_social', 'cpf', 'email',
+                    'tipo', 'alojamento', 'deficiencia')
     template_name_suffix = '_print_list'
     paginate_by = False
 
     def get_table_class(self):
         extra = {
             'nome_completo': Column(accessor='usuario.nome_completo'),
+            'nome_social': Column(accessor='usuario.nome_social'),
+            'cpf': Column(accessor='usuario.cpf'),
             'email': Column(accessor='usuario.email')
-
         }
         return table_factory(self.model, self.list_display, action=None,
                              extra=extra)
